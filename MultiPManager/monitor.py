3b798393efbd49b4a22acf19a056d64bef1d0ddf
Juan Sebatian Triana <juan.triana@volantetech.com>
2021-01-25 21:21:10 -0500

    Monitor

--- MultiPManager/monitor.py
+++ MultiPManager/monitor.py
@@ -48,6 +48,13 @@ __status__ = "Development"
 import sys
 import threading
 import time
+from collections import Mapping, Container
+from sys import getsizeof
+from guppy import hpy
+import psutil
+import platform
+from datetime import datetime
+import socket
 
 class StoppableThread(threading.Thread):
     def __init__(self,  *args, **kwargs):
@@ -60,64 +67,62 @@ class StoppableThread(threading.Thread):
 
 class MonitorThread(StoppableThread):
     def run(self):
-        global interesting
+        prevnumres = 0
+        hp=hpy()
+        t0 = int(time.time())
+        global numres
+        global dnumres
+        hp.heap()
         while(not self.stopped()):
-            print interesting
-            time.sleep(5)
-            
-           
-def monitor():
-    global interesting
-    while(1):
-        print interesting
-        time.sleep(5)
+            #print man.numresults
+            t = int(time.time()) - t0
+            tmp =man.numresults
+            numres.append((t,tmp))
+            dnumres.append((t,tmp-prevnumres))
+            prevnumres = tmp
+            time.sleep(1)
+        hp.heap()
+        
+ 
+def deep_getsizeof(o, ids):
+    d = deep_getsizeof
+    if id(o) in ids:
+        return 0
+    r = getsizeof(o)
+    ids.add(id(o))
+    if isinstance(o, str) or isinstance(0, unicode):
+        return r
+    if isinstance(o, Mapping):
+        return r + sum(d(k, ids) + d(v, ids) for k, v in o.iteritems())
+    if isinstance(o, Container):
+        return r + sum(d(x, ids) for x in o)
+    return r 
 
-import multiprocessing
-proc = multiprocessing.Process(target=monitor, args=())
-proc.start()
-# Terminate the process
-proc.terminate()
-            
+platform = platform.uname()
 
+cpufreq = psutil.cpu_freq()
 
-__author__ = "Juan Sebastian Triana Correa"
-__copyright__ = "Copyright 2015, TLON group"
-__license__ = "LGPL"
-__version__ = "1.2"
-__email__ = "justrianaco@unal.edu.co"
-__status__ = "Development"
+cpu={
+	'phcores':psutil.cpu_count(logical=False),
+	'cores':psutil.cpu_count(logical=True),
+	'freqMax':cpufreq.max,
+	'freqMin':cpufreq.min,
+	'freqCur':cpufreq.current,
+	'usage':psutil.cpu_percent()
+}
 
-import sys
-import threading
-import time
+svmem = psutil.virtual_memory()
 
-class StoppableThread(threading.Thread):
-    def __init__(self,  *args, **kwargs):
-        super(StoppableThread, self).__init__(*args, **kwargs)
-        self._stop_event = threading.Event()
-    def stop(self):
-        self._stop_event.set()
-    def stopped(self):
-        return self._stop_event.is_set()
-
-class MonitorThread(StoppableThread):
-    def run(self):
-        global numresults
-        while(not self.stopped()):
-            print numresults
-            time.sleep(5)
-            
-p.MonitorThread()
-           
-def monitor():
-    global interesting
-    while(1):
-        print interesting
-        time.sleep(5)
-
-import multiprocessing
-proc = multiprocessing.Process(target=monitor, args=())
-proc.start()
-# Terminate the process
-proc.terminate()
-            
+memory={
+	'total':    round(svmem.total/1000000000.0,2),
+	'available':round(svmem.available/1000000000.0,2),
+	'used':     round(svmem.used/1000000000.0,2),
+	'percent':  svmem.percent
+}
+	
+nodeDesc = {
+			'node':socket.gethostbyname(socket.gethostname()),
+			'platform':platform,
+			'cpu':cpu,
+			'memory':memory
+			}
